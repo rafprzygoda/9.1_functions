20bbfaf111e1d12bc58358060c2a9e4f85202940
rafal <ra88fi@o2.pl>
2017-05-31 19:20:49 +0200

    script fixed, add 'or' statement and delete else

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,12 @@ var triangle2Area = getTriangleArea(5, 6);
 var triangle3Area = getTriangleArea(0, 0);  // Show what happens if both parameters will be 0
 
 function getTriangleArea(a, h) {
-    if (a <= 0 && h <= 0) {
+    if (a <= 0 || h <= 0) {
         return 'Wrong data';
-    } else {
+    }  
         return a * h / 2;
     }
-}
 
-console.log(triangle1Area); // h = 0
-console.log(triangle2Area);
-console.log(triangle3Area); // a & h = 0
+console.log(triangle1Area); // Wrong data
+console.log(triangle2Area); // Correct
+console.log(triangle3Area); // Wrong data
